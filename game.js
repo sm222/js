c667618b79520ad19ae62f3471527d257b3c4950
sm222 <soleil.maraska@gmail.com>
2024-08-11 14:09:53 -0400

    almost v1 of game

--- game.js
+++ game.js
@@ -1,5 +1,157 @@
 import * as THREE from 'three'
+import { scene, camera } from './main.js'
+import { Box } from './box.js'
 
-let round = 0
-let mode  = 0
+let Round = 0
+let Mode  = 0
 
+
+export let   GameSize = 15
+export let   PlayerSpeed = 0.15
+export let   Players = []
+export let   Map     = []
+export let   Ball    = []
+export let   Light   = []
+
+export const keys = {
+  a: {
+    pressed: false
+  },
+  d: {
+    pressed: false
+  },
+  left: {
+    pressed: false
+  },
+  right: {
+    pressed: false
+  },
+  space: {
+    pressed: false
+  }
+}
+
+window.addEventListener('keydown', (event) => {
+      switch (event.code) {
+    case 'KeyA':
+      keys.a.pressed = true
+      break
+    case 'KeyD':
+      keys.d.pressed = true
+      break
+    case 'ArrowLeft':
+      keys.left.pressed = true
+      break
+    case 'ArrowRight':
+      keys.right.pressed = true
+      break
+    case 'Space':
+      stop = true
+      break
+  }
+})
+
+window.addEventListener('keyup', (event) => {
+  switch (event.code) {
+    case 'KeyA':
+      keys.a.pressed = false
+      break
+    case 'KeyD':
+      keys.d.pressed = false
+      break
+    case 'ArrowLeft':
+      keys.left.pressed = false
+      break
+    case 'ArrowRight':
+      keys.right.pressed = false
+      break
+  }
+})
+
+
+export function initGame(gamesize) {
+  GameSize = gamesize
+  PlayerSpeed = 0.15
+  Round = 0
+  //
+  Map[0] = new Box({
+    width: GameSize,
+    height: 0.5,
+    depth: GameSize,
+    color: '#0369a1',
+    position: {
+      x: 0,
+      y: -2,
+      z: 0
+    }
+  })
+  // player
+  Players[0] = new Box({
+    width: 2,
+    height: 0.4,
+    depth: 0.4,
+    velocity: {
+      x: 0,
+      y: -0.01,
+      z: 0
+    },
+    position: {
+      x: 0,
+      y: 0.5,
+      z: (GameSize / 2) - (0.5 / 2)
+    }
+  })
+  
+  Players[1] = new Box({
+    width: 2,
+    height: 0.4,
+    depth: 0.3,
+    color: 'red',
+    velocity: {
+      x: 0,
+      y: -0.01,
+      z: 0
+    },
+    position: {
+      x: 0,
+      y: 0.5,
+      z: ((GameSize / 2) * - 1 ) + (0.5 / 2)
+    }
+  })
+  //
+  Light[0] = new THREE.DirectionalLight(0xffffff, 1)
+  Light[0].position.y = 3
+  Light[0].position.z = 1
+  Light[0].castShadow = true
+
+  //
+  scene.add(new THREE.AmbientLight(0xffffff, 0.5))
+  Light.forEach(light => {
+    light.castShadow = true
+    scene.add(light)
+  })
+  //
+  Players.forEach(player => {
+    player.castShadow = true
+    scene.add(player)
+  })
+  //
+  Map.forEach(obj => {
+    obj.receiveShadow = true
+    scene.add(obj)
+  })
+  camera.position.set(Map[0].position.x / 2, Map[0].position.y + GameSize, Map[0].position.z / 2)
+  camera.lookAt(Map[0].position)
+  //Gaming(gamemode)
+}
+
+let GameLoop = 1
+
+function Gaming() {
+  if (GameLoop > 0)
+    requestAnimationFrame(Gaming)
+  //renderer.render(scene, camera)
+}
+
+
+// > call menu here
--- main.js
+++ main.js
@@ -3,12 +3,13 @@ import { OrbitControls } from 'three/addons/controls/OrbitControls'
 import { Box, boxCollision } from './box.js'
 import {ball} from './ball.js'
 import { Obj } from './obj.js'
+import { initGame, Map, Players, GameSize, PlayerSpeed, keys } from './game.js'
 
 //https://youtu.be/sPereCgQnWQ?si=8OPsM8BTY7RlDg4E
 
 
-const scene = new THREE.Scene()
-const camera = new THREE.PerspectiveCamera(
+export const scene = new THREE.Scene()
+export const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
@@ -19,206 +20,104 @@ const renderer = new THREE.WebGLRenderer({
   alpha: true,
   antialias: true
 })
+
 renderer.shadowMap.enabled = true
 renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 
-const GameSize = 10
-const playerSpeed = 0.15
 
-const ground = new Box({
-  width: GameSize,
-  height: 0.5,
-  depth: GameSize,
-  color: '#0369a1',
-  position: {
-    x: 0,
-    y: -2,
-    z: 0
-  }
-})
 
-const players = [];
+//
+initGame(10)
 
-players[0] = new Box({
-  width: 2,
-  height: 0.4,
-  depth: 0.5,
-    velocity: {
-      x: 0,
-      y: -0.01,
-      z: 0
-    },
-    position: {
-      x: 0,
-      y: 0.5,
-      z: (GameSize / 2) - (0.5 / 2)
-    }
-  })
-  
-  players[1] = new Box({
-    width: 2,
-  height: 0.4,
-  depth: 0.5,
-  color: 'red',
-    velocity: {
-      x: 0,
-      y: -0.01,
-      z: 0
-    },
-    position: {
-      x: 0,
-      y: 0.5,
-      z: ((GameSize / 2) * - 1 ) + (0.5 / 2)
-    }
-  })
-  
-  players.forEach(player => {
-    player.castShadow = true
-    scene.add(player)
-})
+var stop = false
 
-//players[0].castShadow = true
-//scene.add(players[0])
 
-ground.receiveShadow = true
-scene.add(ground)
-camera.position.set(ground.position.x / 2, ground.position.y + 10, ground.position.z / 2)
-camera.lookAt(ground.position)
 
-const light = new THREE.DirectionalLight(0xffffff, 1)
-light.position.y = 3
-light.position.z = 1
-light.castShadow = true
-scene.add(light)
-
-scene.add(new THREE.AmbientLight(0xffffff, 0.5))
-
-console.log(ground.top)
-console.log(players[0].bottom)
-
-const keys = {
-  a: {
-    pressed: false
-  },
-  d: {
-    pressed: false
-  },
-  left: {
-    pressed: false
-  },
-  right: {
-    pressed: false
-  }
-}
-
-window.addEventListener('keydown', (event) => {
-  switch (event.code) {
-    case 'KeyA':
-      keys.a.pressed = true
-      break
-    case 'KeyD':
-      keys.d.pressed = true
-      break
-    case 'ArrowLeft':
-      keys.left.pressed = true
-      break
-    case 'ArrowRight':
-      keys.right.pressed = true
-      break
-    //case 'Space':
-    //  players[0].velocity.y = 0.08
-    //  break
-  }
-})
-
-window.addEventListener('keyup', (event) => {
-  switch (event.code) {
-    case 'KeyA':
-      keys.a.pressed = false
-      break
-    case 'KeyD':
-      keys.d.pressed = false
-      break
-    case 'ArrowLeft':
-        keys.left.pressed = false
-        break
-    case 'ArrowRight':
-      keys.right.pressed = false
-      break
-  }
-})
-
-const enemies = []
-
-let frames = 0
-let spawnRate = 200
+//let frames = 0
+//let spawnRate = 200
 function animate() {
-  const animationId = requestAnimationFrame(animate)
+  if (!stop)
+    requestAnimationFrame(animate)
   renderer.render(scene, camera)
-
+  
   // movement code
-  players.forEach(player => {
+  Players.forEach(player => {
     player.velocity.x = 0
   })
   
-  if (keys.a.pressed && players[0].position.x > (GameSize / 2) * -1 + (players[0].width / 2))
-    players[0].velocity.x = playerSpeed * -1
-  else if (keys.d.pressed && players[0].position.x < (GameSize / 2) - (players[0].width / 2))
-    players[0].velocity.x = playerSpeed
-  
-  if (keys.left.pressed) {
-    players[1].velocity.x = playerSpeed
+  if (keys.a.pressed && Players[0].position.x > (GameSize / 2) * -1 + (Players[0].width / 2)) {
+    Players[0].velocity.x = PlayerSpeed * -1
   }
-  else if (keys.right.pressed) 
-    players[1].velocity.x = playerSpeed * -1
+  else if (keys.d.pressed && Players[0].position.x < (GameSize / 2) - (Players[0].width / 2)) {
+    Players[0].velocity.x = PlayerSpeed
+  }
   
-  players.forEach((player) => {
-    player.update(ground)
-  })
-  
-  enemies.forEach((enemy) => {
-    enemy.update(ground)
-    if (
-      boxCollision({
-        box1: players[0],
-        box2: enemy
-      })
-    ) {
-      cancelAnimationFrame(animationId)
-    }
-  })
-
-  if (frames % spawnRate === 0) {
-    if (spawnRate > 20) spawnRate -= 20
-
-    const enemy = new ball({
-      width: 2,
-      height: 2,
-      depth: 2,
-      color: 'red',
-      velocity: {
-        x: 0,
-        y: 0,
-        z: 0.005
-      },
-      position: {
-        x: (Math.random() - 0.5) * 10,
-        y: 0,
-        z: -20
-      },
-      zAcceleration: true
-    })
-    enemy.castShadow = true
-    scene.add(enemy)
-    enemies.push(enemy)
+  if (keys.left.pressed && Players[1].position.x > (GameSize / 2) * -1 + (Players[1].width / 2)) {
+    Players[1].velocity.x = PlayerSpeed * -1
+  }
+  else if (keys.right.pressed && Players[1].position.x < (GameSize / 2) - (Players[1].width / 2)) {
+    Players[1].velocity.x = PlayerSpeed
   }
 
-  frames++
-  // players[0].position.y += -0.01
+  Players.forEach((player) => {
+    player.update(Map[0])
+  })
+}
+
+
+animate()
+
+
+  
+  //    funny gamemode
+  // let ing = Math.atan2(players[1].position.y - players[0].position.y , players[1].position.x - players[0].position.x)
+  // console.log(ing)
+  //camera.rotation.z = ing
+
+
+
+
+  //enemies.forEach((enemy) => {
+    //  enemy.update(ground)
+    //  if (
+      //    boxCollision({
+        //      box1: players[0],
+        //      box2: enemy
+        //    })
+        //  ) {
+  //    cancelAnimationFrame(animationId)
+  //  }
+  //})
+  
+  //if (frames % spawnRate === 0) {
+    //  if (spawnRate > 20) spawnRate -= 20
+    
+    //  const enemy = new ball({
+      //    width: 2,
+      //    height: 2,
+      //    depth: 2,
+      //    color: 'red',
+      //    velocity: {
+        //      x: 0,
+        //      y: 0,
+        //      z: 0.005
+        //    },
+        //    position: {
+          //      x: (Math.random() - 0.5) * 10,
+          //      y: 0,
+          //      z: -20
+          //    },
+          //    zAcceleration: true
+          //  })
+          //  enemy.castShadow = true
+          //  scene.add(enemy)
+          //  enemies.push(enemy)
+          //}
+          
+          //frames++
+          // players[0].position.y += -0.01
   // players[0].rotation.x += 0.01
   // players[0].rotation.y += 0.01
-}
-animate()
